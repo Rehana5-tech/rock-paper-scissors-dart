090be96a1ccb83b51135c8f6e88a3b87ff87bf91
Rehana5-tech <rehanashafi669@gmail.com>
2025-06-04 15:42:35 +0500

    Delete lib/static.dart

--- lib/static.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-class form {
-  static form1(String name, String password) {
-    stdout.write('enter your name:$name');
-    stdout.write('enter your password:$password');
-
-  }
-}
